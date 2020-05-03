5f444645007beb24c813aab9b8b8b5a82e6826ca
Luis Otávio <luis.otavio2425@gmail.com>
2020-05-02 21:14:00 -0300

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
 
 const app = express();
 const server = http.Server(app);
 
+app.use(cors());
 app.use(express.Router());
 app.use(express.json());
 
